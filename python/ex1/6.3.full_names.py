f893198643e513a33d20dbe673ed9d47b6872a79
dov liberman <doliberm@g.jct.ac.il>
2023-03-02 13:10:44 +0200

    add week 6 to py ex1

---
+++ python/ex1/6.3.full_names.py
@@ -0,0 +1,12 @@
+
+def full_names(first_names, last_names, min_len =0):
+    return [ str.title(f_name+" " + l_name) for f_name in first_names for l_name in last_names if len(f_name )+len(l_name)+1 >= min_len ]
+
+
+first_names = ['avi', 'moshe', 'yaakov']
+last_names = ['cohen', 'levi', 'mizrahi']
+
+# התנאים הבאים צריכים להתקיים
+a1 = full_names(first_names, last_names, 10) == ['Avi Mizrahi', 'Moshe Cohen', 'Moshe Levi', 'Moshe Mizrahi', 'Yaakov Cohen', 'Yaakov Levi', 'Yaakov Mizrahi']
+a2 = full_names(first_names, last_names) == ['Avi Cohen', 'Avi Levi', 'Avi Mizrahi', 'Moshe Cohen', 'Moshe Levi', 'Moshe Mizrahi', 'Yaakov Cohen', 'Yaakov Levi', 'Yaakov Mizrahi']
+print(a1 and a2 )
